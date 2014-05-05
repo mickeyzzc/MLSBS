5515c290844ce3599a463670e01017ff8c80585e
mickey_zzc <mickeybee@mickeybee.cn>
2014-05-05 17:55:39 +0800

    add iptables

--- EasyInstall/function/iptables_set.sh
+++ EasyInstall/function/iptables_set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#安装iptables
 INSTALL_BASE_PACKAGES iptables
-
+#检测网卡
 ETHERNET_CHECK(){
 	Eths=${ifconfig|awk '!/^ |^$|lo/ {print $1}'}
 	if [ "$Eths" == '' ] ; then
@@ -9,7 +9,7 @@ ETHERNET_CHECK(){
 		exit 1
 	fi
 }
-
+#iptables的基本设置和备份
 IPTABLES_BASE_SET(){
 	[ ! -d $ScriptPath/backup ] && mkdir $ScriptPath/backup
 	iptables-save > $ScriptPath/backup/iptables.up.rules.backup$(date +"%Y%m%d")
@@ -22,11 +22,11 @@ IPTABLES_BASE_SET(){
 	iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 	iptables -A INPUT -j DROP
 }
-
+#增加放行端口的规则
 IPTABLES_INPUT_SET(){
 	iptables -I INPUT 3 -p tcp -m multiport -dport $1 -j ACCEPT
 }
-
+#输入需要放行的有效的端口号
 IPTABLES_SET_CONFIG(){
 	InputPorts=""
 	InputPort=""
@@ -48,7 +48,7 @@ IPTABLES_SET_CONFIG(){
 			r|R)
 				InputPorts="";;
 			*)
-				echo "input is not number, please input again"
+				echo "input is not number, please input again";;
 		esac
 		echo "your port number is $InputPorts"
 	done
