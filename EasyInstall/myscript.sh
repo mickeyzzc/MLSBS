fcce5b9dd005a4e36d76dbf35aadef473503f224
mickey_zzc <mickeybee@mickeybee.cn>
2014-05-06 16:17:15 +0800

    fix iptables

--- EasyInstall/myscript.sh
+++ EasyInstall/myscript.sh
@@ -34,16 +34,14 @@ SELECT_RUN_SCRIPT(){
 				TEST_SCRIPT mysql_install.sh
 				MYSQL_VAR && MYSQL_BASE_PACKAGES_INSTALL && INSTALL_MYSQL;;
 			"Setup firewall")
-				case $SysVer in
-					@(2.[4-6]|3.[0-9]|3.1[0-2]))
-						TEST_SCRIPT iptables_set.sh
-						SELECT_IPTABLES_FUNCTION;;
-					@(3.1[3-9]|3.[2-9][0-9]))
-						echo "wait my script update";;
-					*)
-						echo "your system is no supported my firewall script";;
-				esac
-				;;
+				if [ ${SysVer%%.*} -eq 3 -a ${SysVer%%.*} -le 12 -o "$SysVer" = "2.4" -o "$SysVer" = "2.6" -o "$SysVer" = "2.5" ];then
+					TEST_SCRIPT iptables_set.sh
+					SELECT_IPTABLES_FUNCTION
+				elif [ ${SysVer%%.*} -eq 3 -a ${SysVer%%.*} -ge 13 ];then
+					echo "wait my script update"
+				else
+					echo "your system is no supported my firewall script"
+				fi;;
 			"Install Puppet")
 				TEST_SCRIPT puppet_install.sh
 				PUPPET_VAR && SELECT_PUPPET_FUNCTION;;
